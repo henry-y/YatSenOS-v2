222aae9e3e024d1f4c154f7b35ba2ff0ee07e8ab
henry-y <2572092963@qq.com>
2024-02-29 19:24:37 +0800

    Progress: finish lab4 2.1

--- pkg/kernel/src/main.rs
+++ pkg/kernel/src/main.rs
@@ -10,14 +10,14 @@ boot::entry_point!(kernel_main);
 
 pub fn kernel_main(boot_info: &'static boot::BootInfo) -> ! {
     ysos::init(boot_info);
-    ysos::wait(spawn_init());
+    // ysos::wait(spawn_init());
     ysos::shutdown(boot_info);
 }
 
-pub fn spawn_init() -> proc::ProcessId {
-    // NOTE: you may want to clear the screen before starting the shell
-    // print_serial!("\x1b[1;1H\x1b[2J");
+// pub fn spawn_init() -> proc::ProcessId {
+//     // NOTE: you may want to clear the screen before starting the shell
+//     // print_serial!("\x1b[1;1H\x1b[2J");
 
-    proc::list_app();
-    proc::spawn("sh").unwrap()
-}
+//     proc::list_app();
+//     proc::spawn("sh").unwrap()
+// }
